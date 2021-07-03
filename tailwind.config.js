e9613983cb94975215fc84cf2ee38c128f7648e6
Victor Miguel Armenta Carrillo <victorarmenta30@gmail.com>
2021-07-02 19:04:40 -0500

    Footer and fix the menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,9 @@ module.exports = {
       },
     },
     theme: {
-      extend: {
-      },
+      fontFamily: {
+        sans: ['"Lato"', 'sans-serif']
+      }
     },
     variants: {},
     plugins: [],
